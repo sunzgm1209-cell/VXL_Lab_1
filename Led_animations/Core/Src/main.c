08626959c4805df57a776fd5a4dcd214bcbb7ab4
sunzgm1209-cell <sunzgm1209@gmail.com>
2026-09-13 20:06:31 +0700

    Cấu hình PA5 và PA6

--- Led_animations/Core/Src/main.c
+++ Led_animations/Core/Src/main.c
@@ -148,10 +148,10 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOA_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, RED_LED_Pin|YELLOW_RED_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, RED_LED_Pin|YELLOW_LED_Pin, GPIO_PIN_RESET);
 
-  /*Configure GPIO pins : RED_LED_Pin YELLOW_RED_Pin */
-  GPIO_InitStruct.Pin = RED_LED_Pin|YELLOW_RED_Pin;
+  /*Configure GPIO pins : RED_LED_Pin YELLOW_LED_Pin */
+  GPIO_InitStruct.Pin = RED_LED_Pin|YELLOW_LED_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
